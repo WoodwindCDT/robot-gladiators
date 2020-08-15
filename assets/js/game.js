7970ae0f34d65cea00f75a8d3f789a465ca8384c
Ender <woodwind.turbeville@gmail.com>
2020-08-15 13:55:25 -0500

    Added easier prompt input for shop and simplified input response needed to only a digit

--- assets/js/game.js
+++ assets/js/game.js
@@ -156,22 +156,20 @@ var endGame = function() {
 var shop = function() {
     // To ask what player wants to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
     // Switch to continue shop option
-    switch (shopOptionPrompt) {
-        case "REFILL": 
-        case "refill":
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+    switch (shopOptionPrompt) { 
+        case 1:
             playerInfo.refillHealth();
             break;
 
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
             
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
 
             // Does nothing, so function can end
